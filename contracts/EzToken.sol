ef1dbdf8c7826329ecca79c1cb2dbafb5b0dccb1
eztoken <dev@ezpos.io>
2018-02-12 13:04:19 +0700

    Require amount zero before approval

--- contracts/EzToken.sol
+++ contracts/EzToken.sol
@@ -183,6 +183,7 @@ contract EZToken {
      */
     function approve(address _spender, uint256 _value) public
         returns (bool success) {
+        require((_value == 0) || (allowed[msg.sender][_spender] == 0));
         allowed[msg.sender][_spender] = _value;
         Approval(msg.sender, _spender, _value);
         return true;
