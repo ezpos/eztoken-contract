0524f0858a93a723b7af452a6e0dd2a0559f05bf
eztoken <dev@ezpos.io>
2018-02-12 11:16:10 +0700

    Remove approveAndCall

--- contracts/EzToken.sol
+++ contracts/EzToken.sol
@@ -205,24 +205,7 @@ contract EZToken {
         return true;
     }
 
-    /**
-     * Set allowed for other address and notify
-     *
-     * Allows `_spender` to spend no more than `_value` tokens on your behalf, and then ping the contract about it
-     *
-     * @param _spender The address authorized to spend
-     * @param _value the max amount they can spend
-     * @param _extraData some extra information to send to the approved contract
-     */
-    function approveAndCall(address _spender, uint256 _value, bytes _extraData)
-        public
-        returns (bool success) {
-        tokenRecipient spender = tokenRecipient(_spender);
-        if (approve(_spender, _value)) {
-            spender.receiveApproval(msg.sender, _value, this, _extraData);
-            return true;
-        }
-    }
+    
 
     /**
      * Destroy tokens
